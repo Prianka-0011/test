1981eb7bd2c3c8fbfbdb352b1ca353cce294a693
pranka mondal <priankanew0011@gmail.com>
2021-03-08 10:35:33 +0600

    staticteamplatedone

--- js/main.js
+++ js/main.js
@@ -20,15 +20,20 @@ $(window).on('scroll',function(){
    });
 //end
 var mybutton = document.getElementById("scroll-top");
+var mymsg= document.getElementById("visibleafterscrollmsg");
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
     mybutton.style.display = "block";
     mybutton.addClass="scrollingtop-active"
-
-  } else {
+    
+  } 
+  else {
     mybutton.style.display = "none";
+    mymsg.style.display = "none";
+    
   }
 }
 
@@ -44,16 +49,21 @@ $('#topFunction').click(function() {
   return false;
   });
 
-  $(function() {
-     
+  $(function() {    
       $('.dropdown').has('.dropdown-menu')
       .mouseover(function(){
         $(this).children('.dropdown-menu').css('visibility','visible');
       })
       .mouseout(function(){
         $(this).children('.dropdown-menu').css('visibility','hidden');
-      })
-    
+      })    
   });
   //image carsoul auto play
   $('.carousel').carousel()
+
+ 
+
+  $('#msgFunction').click(function() {
+    $('.visibleafterscrollmsg').toggle('showmsg');
+    return false;
+    });
